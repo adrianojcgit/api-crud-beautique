9831f07e07441a53f1bf431ff3da9f597f74034b
Adriano Cordeiro <adrianojcordeiro@gmail.com>
2025-10-17 17:01:43 -0300

    Alterar hieraquia das pastas do projeto.

---
+++ ms-beautique/src/main/java/br/com/beautique/api/dtos/FullAppointmentDTO.java
@@ -0,0 +1,23 @@
+package br.com.beautique.api.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class FullAppointmentDTO {
+
+    private Long id;
+    private LocalDateTime dateTime;
+    private Boolean appointmentsOpen;
+
+    private CustomerDTO customer;
+    private BeautyProcedureDTO beautyProcedureDTO;
+
+}
---
+++ ms-beautique/src/main/java/br/com/beautique/api/services/CustomerService.java
@@ -0,0 +1,9 @@
+package br.com.beautique.api.services;
+
+import br.com.beautique.api.dtos.CustomerDTO;
+
+public interface CustomerService {
+    CustomerDTO create(CustomerDTO customerEntity);
+    void delete(Long id);
+    CustomerDTO update(CustomerDTO customerDTO);
+}
