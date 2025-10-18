581daca286eb8e3b3691063487c46fd6ad2fbb22
Adriano Cordeiro <adrianojcordeiro@gmail.com>
2025-10-18 10:33:58 -0300

    Implementatio layers.

---
+++ ms-sync/src/main/java/br/com/beautique/ms_sync/services/impl/AppointmentServiceImpl.java
@@ -0,0 +1,23 @@
+package br.com.beautique.ms_sync.services.impl;
+
+import br.com.beautique.ms_sync.dtos.appointments.FullAppointmentDTO;
+import br.com.beautique.ms_sync.dtos.beautyprocedures.BeautyProcedureDTO;
+import br.com.beautique.ms_sync.dtos.customers.CustomerDTO;
+import br.com.beautique.ms_sync.services.AppointmentService;
+
+public class AppointmentServiceImpl implements AppointmentService {
+    @Override
+    public void saveAppointment(FullAppointmentDTO appointment) {
+
+    }
+
+    @Override
+    public void updateAppointmentCustomer(CustomerDTO customer) {
+
+    }
+
+    @Override
+    public void updateAppointmentBeautyProcedures(BeautyProcedureDTO beautyProcedure) {
+
+    }
+}
