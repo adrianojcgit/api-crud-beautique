57fccd0b54292c9862a1ff62166ac72a301a6a32
Adriano Cordeiro <adrianojcordeiro@gmail.com>
2025-10-18 11:44:26 -0300

    Implementation Synchro interface.

--- ms-sync/src/main/java/br/com/beautique/ms_sync/services/impl/SyncServiceImpl.java
+++ ms-sync/src/main/java/br/com/beautique/ms_sync/services/impl/SyncServiceImpl.java
@@ -3,23 +3,62 @@ package br.com.beautique.ms_sync.services.impl;
 import br.com.beautique.ms_sync.dtos.appointments.FullAppointmentDTO;
 import br.com.beautique.ms_sync.dtos.beautyprocedures.BeautyProcedureDTO;
 import br.com.beautique.ms_sync.dtos.customers.CustomerDTO;
+import br.com.beautique.ms_sync.services.AppointmentService;
+import br.com.beautique.ms_sync.services.BeautyProcedureService;
+import br.com.beautique.ms_sync.services.CustomerService;
 import br.com.beautique.ms_sync.services.SyncService;
+import br.com.beautique.ms_sync.utils.SyncLogger;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 public class SyncServiceImpl implements SyncService {
-    @Override
-    public void syncCustomer(CustomerDTO customer) {
 
+    private final AppointmentService appointmentService;
+    private final CustomerService customerService;
+    private final BeautyProcedureService beautyProcedureService;
+
+    public SyncServiceImpl(AppointmentService appointmentService, CustomerService customerService, BeautyProcedureService beautyProcedureService) {
+        this.appointmentService = appointmentService;
+        this.customerService = customerService;
+        this.beautyProcedureService = beautyProcedureService;
     }
 
     @Override
-    public void syncAppointment(FullAppointmentDTO fullAppointment) {
-
+    public void syncCustomer(CustomerDTO customerDTO) {
+        try{
+            SyncLogger.info("Saving customer: " + customerDTO.getId());
+            customerService.saveCustomer(customerDTO);
+            SyncLogger.info("Updating appointment customer: " + customerDTO.getId());
+            appointmentService.updateAppointmentCustomer(customerDTO);
+        }catch(Exception e){
+               SyncLogger.error("Error saving customer: " + e.getMessage());
+               SyncLogger.trace(Arrays.toString(e.getStackTrace()));
+        }
     }
 
     @Override
-    public void syncBeautyProcedures(BeautyProcedureDTO beautyProcedure) {
+    public void syncAppointment(FullAppointmentDTO fullAppointmentDTO) {
+        try{
+            SyncLogger.info("Saving appointment: " + fullAppointmentDTO.getId());
+            appointmentService.saveAppointment(fullAppointmentDTO);
+        }catch(Exception e){
+            SyncLogger.error("Error saving appointment: " + e.getMessage());
+            SyncLogger.trace(Arrays.toString(e.getStackTrace()));
+        }
+    }
 
+    @Override
+    public void syncBeautyProcedures(BeautyProcedureDTO beautyProcedureDTO) {
+        try{
+            SyncLogger.info("Saving beauty procedure: " + beautyProcedureDTO.getId());
+            beautyProcedureService.saveBeautyProcedure(beautyProcedureDTO);
+            SyncLogger.info("Updating appointment beauty procedure: " + beautyProcedureDTO.getId());
+            appointmentService.updateAppointmentBeautyProcedures(beautyProcedureDTO);
+        }catch(Exception e){
+            SyncLogger.error("Error saving beauty procedure: " + e.getMessage());
+            SyncLogger.trace(Arrays.toString(e.getStackTrace()));
+        }
     }
 }
